ba6b10d556d80d22a2e9023c4f3709ba0b7390d9
JhonatanIT <jhonatan.ibatac@gmail.com>
2023-02-02 02:25:38 -0500

    feat: add async client Discord

--- run.py
+++ run.py
@@ -1,12 +1,17 @@
 from flask import Flask
 from app.discord_bot.discord_api import client, discord_token
+import threading
 
 app = Flask(__name__)
 
+def callClientDiscord():
+     if not client.application:
+        client.run(discord_token)
+
 @app.route("/")
-def hello_world():  
-    #client.run(discord_token)
-    return "<p>Hello, World!</p>"
+def chatgptBot():  
+    threading.Thread(target=callClientDiscord).start()
+    return "<p>ChatGPT Bot activated!</p>"
 
 if __name__ == '__main__':
     app.run()
