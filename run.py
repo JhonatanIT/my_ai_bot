16c28dea3a20b9546772087caa691749868b6053
JhonatanIT <jhonatan.ibatac@gmail.com>
2023-02-02 01:51:01 -0500

    build: add flask

--- run.py
+++ run.py
@@ -1,4 +1,14 @@
+from flask import Flask
 from app.discord_bot.discord_api import client, discord_token
 
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():  
+    #client.run(discord_token)
+    return "<p>Hello, World!</p>"
+
 if __name__ == '__main__':
-    client.run(discord_token)
+    app.run()
+    
+
